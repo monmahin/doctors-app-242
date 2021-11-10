ba4c12c14a37db987ebbf4eb7272eb6a9203a81d
monmahin <monmahin13@gmail.com>
2021-11-11 05:36:52 +0600

    doctors-app-server-2

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
 //get appointment data from mongodb
         app.get('/appointments',verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date
 
             const query = { email: email, date: date }
 
